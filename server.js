3ce31679ee1702bd7fc4dd6746b21970f83b6a62
codersrabbi <pcrabbi2020@gmail.com>
2026-04-18 23:27:39 +0600

    node modules ignore done

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// ================== MIDDLEWARE ==================
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdtg0ka.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const allBlogs = client.db("al-arafat-foundation").collection("blogs");
+
+
+        app.get("/blogs", async (req, res) => {
+            const result = await allBlogs.find().toArray();
+            res.send(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged MongoDB successfully!");
+
+    } finally {
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
